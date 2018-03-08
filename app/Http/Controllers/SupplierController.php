bfa65eff4612311154486457b7c3c47e4bc2df93
ree_file <1205730728@qq.com>
2018-03-08 05:18:21 +0000

    12331

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -42,17 +42,18 @@ class SupplierController extends Controller
       $OrderGoods->order_id = $new_order->id;
       $OrderGoods->goods_content = serialize($order['goods']);
       $OrderGoods->save();
+      return $this->success($order['goods']);
       // 创建支付单。
-    	$alipay = app('alipay.web');
-    	$alipay->setOutTradeNo($order['order_code']);
-    	$alipay->setTotalFee($order['order_pay']);
-    	$alipay->setSubject('宅校');
-    	$alipay->setBody('goods_description');
-
-      $alipay->setQrPayMode('4'); //该设置为可选，添加该参数设置，支持二维码支付。
-
-    	// // 跳转到支付页面。
-    	return redirect()->to($alipay->getPayLink());
+    	// $alipay = app('alipay.web');
+    	// $alipay->setOutTradeNo($order['order_code']);
+    	// $alipay->setTotalFee($order['order_pay']);
+    	// $alipay->setSubject('宅校');
+    	// $alipay->setBody('goods_description');
+      //
+      // $alipay->setQrPayMode('4'); //该设置为可选，添加该参数设置，支持二维码支付。
+      //
+    	// // // 跳转到支付页面。
+    	// return redirect()->to($alipay->getPayLink());
     }
     public function operate(Request $request)
     {
