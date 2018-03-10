60cc21ca684e6ddacdb7c99f7e11c679a47a4db3
ree_file <1205730728@qq.com>
2018-03-10 04:05:14 +0000

    123

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -35,7 +35,7 @@ class SupplierController extends Controller
     public function buy(Request $request)
     {
       $user = User::where("openid",$request->openid)->get();
-      if ($user->isEmpty()) {
+      if ($user->id==null) {
         $zhaiUser = DB::table("ecs_weixin_user")->where("fake_id",$request->openid)->first();
         $user = new User;
         $user->openid = $zhaiUser->fake_id;
